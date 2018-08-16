cc75867c046717e2de099c797911d0db02bc5b1e
Michelli Chaves Vieira <michellicvieira@gmail.com>
2018-08-16 15:17:38 -0500

    done

---
+++ node/keys.js
@@ -0,0 +1,8 @@
+console.log('loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'dEyGMrtbI3VZQxtQmh293Uewc',
+    consumer_secret: 'ThF3e86qGlW4BI2S8tPxpgb8GhK8ZSEGU5SlWOUkkikDDVhoTj',
+    access_token_key: '49153494-rAu7BEwrjZiDSdcMPQtPX8D4bLaqyJCKiM4GbFGGh',
+    access_token_secret: 'zlhSKPPjIDCQFZc2iArsytqufYCJflROq0NCAYZE396nO'
+};
